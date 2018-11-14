bb8151d643fe91568440d1d32442a293fa852791
piscythe <malfunctioningadamantium@gmail.com>
2018-11-13 20:00:24 -0800

    landing page update

---
+++ timber.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head><link rel="stylesheet" type="text/css" href="basic.css"></head>
+<body>
+
+
+<?php
+  session_start();
+  if(empty($_SESSION["username"]) || empty($_SESSION["valid"])) {
+    //user is not logged in
+    echo "<h2>Welcome to Timber!</h2><a href=\"login.php\">Login</a></br><a href=\"user_form.php\">Sign Up</a>";
+  }
+  else {
+    //this should redirect to user profile
+    header("Location: showLogin.php");
+  }
+  
+?>
+
+
+</body>
+</head>
