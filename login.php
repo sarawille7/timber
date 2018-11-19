aa60f99916874900740730b1a4b69ce97b41ba47
David-S-Smith <yinyangbell@gmail.com>
2018-11-19 13:07:29 -0800

    colour tweaks to menu and added menu to some pages

--- login.php
+++ login.php
@@ -2,13 +2,17 @@
 <html>
 <head><link rel="stylesheet" type="text/css" href="basic.css"></head>
 <body>
+<?php
+include("menu.PHP");
+?>
 
-<h2>Login</h2>
-
-<form action="loginProcess.php" method="post">
-    Username:<br><input type="text" name="username" required pattern="[A-Z]\w+|[a-z]\w+"><br>
-    Password:<br><input type="password" name="password"><br>
-<input type="submit" value= '<?php echo "Login"; ?>'>
-</form>
+<div class = "main">
+  <h2>Login</h2>
+  <form action="loginProcess.php" method="post">
+      Username:<br><input type="text" name="username" required pattern="[A-Z]\w+|[a-z]\w+"><br>
+      Password:<br><input type="password" name="password"><br>
+  <input type="submit" value= '<?php echo "Login"; ?>'>
+  </form>
+</div>
 </body>
 </html>
--- timber.php
+++ timber.php
@@ -2,20 +2,26 @@
 <html>
 <head><link rel="stylesheet" type="text/css" href="basic.css"></head>
 <body>
+  <?php
+  include("menu.PHP");
+  ?>
 
+  <div class = "main">
 
-<?php
-  session_start();
-  if(empty($_SESSION["username"]) || empty($_SESSION["valid"])) {
-    //user is not logged in
-    echo "<h2>Welcome to Timber!</h2><a href=\"login.php\">Login</a></br><a href=\"user_form.php\">Sign Up</a>";
-  }
-  else {
-    //this should redirect to user profile
-    header("Location: showLogin.php");
-  }
-  
-?>
+    <?php
+
+      session_start();
+      if(empty($_SESSION["username"]) || empty($_SESSION["valid"])) {
+        //user is not logged in
+        echo "<h2>Welcome to Timber!</h2><a href=\"login.php\">Login</a></br><a href=\"user_form.php\">Sign Up</a>";
+      }
+      else {
+        //this should redirect to user profile
+        header("Location: showLogin.php");
+      }
+
+    ?>
+  </div>
 
 
 </body>
