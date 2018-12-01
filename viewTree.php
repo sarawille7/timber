92bf227ed0b3107f527efee77949f2ace6f88be9
David-S-Smith <yinyangbell@gmail.com>
2018-11-30 18:49:51 -0800

    fixing menu on viewtree

--- viewTree.php
+++ viewTree.php
@@ -9,25 +9,6 @@ if(!isset($_GET["treeID"])) {
 <!DOCTYPE html>
 <html>
 <head>
-	<style>
-		*{
-			box-sizing: border-box;
-		}
-			/* Create two equal columns that floats next to each other */
-		.col {
-		float: left; /*TODO: probably need to fix*/
-		width: 40%; /* was 50 in example */
-		padding: 10px;
-		height: 300px; /* Should be removed. Only for demonstration */
-		}
-
-		/* Clear floats after the columns */
-		.row:after {
-		content: "";
-		display: table;
-		clear: both;
-		}
-	</style>
 </head>
 <body>
 <?php
