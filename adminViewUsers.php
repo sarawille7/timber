0ad6fd4db3f73b587c50567119aa88a7c4dc46e7
piscythe <malfunctioningadamantium@gmail.com>
2018-12-01 19:49:55 -0800

    add ban interface for admins

--- adminViewUsers.php
+++ adminViewUsers.php
@@ -25,7 +25,7 @@ include("menu.PHP");
       foreach ($result_set as $result){
         echo "user: <b>$result[username]</b> <br/>";
         if ($result['privileges'] == "admin"){
-          echo "Is an admin.<br/>";
+          echo "Is an admin.<br/><br/>";
         }
         else {
           $stmt = $db->prepare('SELECT treeID, name FROM trees WHERE username == ?');
@@ -37,6 +37,38 @@ include("menu.PHP");
             echo "Submitted trees:<br/>";
             foreach($result_set as $tuple) {
               echo "<a href= \"viewTree.php?treeID=$tuple[treeID]\">$tuple[name]</a><br/>";
+            }
+          }
+          $stmt = $db->prepare('SELECT banDate FROM banned WHERE username == ?');
+          $stmt->execute(array($result['username']));
+          $result_set = $stmt->fetchAll();
+          if (count($result_set) == 0){
+            echo "User has never been banned. Ban for: 
+              <a  href= \"createBan.php?username=$result[username]&banType=day\">1 Day?</a>  
+              <a  href= \"createBan.php?username=$result[username]&banType=week\">1 Week?</a>  
+              <a  href= \"createBan.php?username=$result[username]&banType=month\">1 Month?</a>  
+              <a  href= \"createBan.php?username=$result[username]&banType=year\">1 Year?</a>  
+              <a  href= \"createBan.php?username=$result[username]&banType=lifetime\">1000 Years?</a>
+              <br/>";
+          }else {
+            $ban = "";
+            foreach($result_set as $tuple) {
+              if ($tuple['banDate'] > $ban){
+                $ban = $tuple['banDate'];
+              }
+            }
+            $currentTime = new DateTime("NOW");
+            $now = $currentTime->format('Y-m-d H:i:s');
+            if ($ban > $now){
+              echo "User is banned until: $ban. <a  href= \"unban.php?username=$result[username]\">Unban?</a><br/>";
+            } else {
+              echo "User's ban ended on: $ban. Ban for: 
+              <a  href= \"createBan.php?username=$result[username]&banType=day\">1 Day?</a>  
+              <a  href= \"createBan.php?username=$result[username]&banType=week\">1 Week?</a>  
+              <a  href= \"createBan.php?username=$result[username]&banType=month\">1 Month?</a>  
+              <a  href= \"createBan.php?username=$result[username]&banType=year\">1 Year?</a>  
+              <a  href= \"createBan.php?username=$result[username]&banType=lifetime\">1000 Years?</a>
+              <br/>";
             }
           }
           echo "<br/>";
---
+++ createBan.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+if($_SESSION["privileges"] != "admin") {
+	//this is an admin-only page
+	header("Location: timber.php");
+	die();
+}
+if(empty($_GET["username"]) || empty($_GET["banType"])) {
+	//redirect back using header()
+	header("Location: adminViewUsers.php");
+	die();
+}
+try{
+
+	$db = new PDO('sqlite:./myDB/timber.db');
+	$db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  $banUser = $_GET["username"];
+  $banType = $_GET["banType"];
+	$currentTime = new DateTime("NOW");
+  $banLength = new DateInterval("PT1M");
+  if ($banType == "day") {
+    $banLength = new DateInterval("P1D");
+  } else if ($banType == "week") {
+    $banLength = new DateInterval("P7D");
+  } else if ($banType == "month") {
+    $banLength = new DateInterval("P28D");
+  } else if ($banType == "year") {
+    $banLength = new DateInterval("P365D");
+  } else if ($banType == "lifetime") {
+    $banLength = new DateInterval("P365000D");
+  }
+	$banDate = $currentTime->add($banLength)->format('Y-m-d H:i:s');
+  $stmt = $db->prepare('SELECT banDate FROM banned WHERE username == ?');
+  $stmt->execute(array($_GET['username']));
+  $result_set = $stmt->fetchAll();
+  if (count($result_set) == 0){
+    $stmt = $db->prepare('INSERT INTO Banned VALUES(?, ?)');
+    $stmt->execute(array($banUser, $banDate));
+  } else {
+    $stmt = $db->prepare('UPDATE Banned SET banDate = ? WHERE username = ?');
+    $stmt->execute(array($banDate, $banUser));
+  }
+	
+	$db = null;
+} catch(PDOException $e) {
+	die('Exception : '.$e->getMessage());
+}
+
+header("Location: adminViewUsers.php");
+?>
