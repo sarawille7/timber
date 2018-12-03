2d2667c96cbb022ab06ef0dd9270fb3136696431
Benjamin Scarbrough <Scarbrough.ben@gmail.com>
2018-12-03 12:42:18 -0800

    forgot something

--- noUser.php
+++ noUser.php
@@ -12,6 +12,7 @@ include("menu.PHP");
   <h2 href="login.php"> Try Again </h2>
 <br>
 <h2 "New here? Click" <h2 href="tree_form.php"> here </h2> to make an account! </h2>
+</div>
 
 </body>
 </html>
