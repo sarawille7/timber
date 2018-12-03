a1e14a40704814e0d648af0f3d71940b6f33cb40
Benjamin Scarbrough <Scarbrough.ben@gmail.com>
2018-12-03 12:43:41 -0800

    forgot something

--- noUser.php
+++ noUser.php
@@ -2,16 +2,13 @@
 <html>
 <head><link rel="stylesheet" type="text/css" href="basic.css"></head>
 <body>
-<?php
-include("menu.PHP");
-?>
 
 <div class = "main">
-  <h1> "Error: Something went wrong!" </h1>
+  <h1> Error: Something went wrong! </h1>
 <br>
   <h2 href="login.php"> Try Again </h2>
 <br>
-<h2 "New here? Click" <h2 href="tree_form.php"> here </h2> to make an account! </h2>
+<h2> "New here? Click" <h2 href="tree_form.php"> here </h2> to make an account! </h2>
 </div>
 
 </body>
