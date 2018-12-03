250859dce3b97c482cfe9d1d60ca35397868ac81
Benjamin Scarbrough <Scarbrough.ben@gmail.com>
2018-12-03 12:44:40 -0800

    forgot something

--- noUser.php
+++ noUser.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html>
-<head><link rel="stylesheet" type="text/css" href="basic.css"></head>
 <body>
+<?php
+include("menu.PHP");
+?>
 
 <div class = "main">
   <h1> Error: Something went wrong! </h1>
