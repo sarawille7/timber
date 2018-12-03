0e12b08059602e7ffc086a21d7d9036271f29b9b
Benjamin Scarbrough <Scarbrough.ben@gmail.com>
2018-12-03 13:10:26 -0800

    forgot something

--- noUser.php
+++ noUser.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html>
-<body>
+<body style="background-color: #749f5d;">
 <?php
 include("menu.PHP");
 ?>
-<style>
 <div class = "main">
 background-color: #749f5d;
   <h1> Error: Something went wrong! </h1>
@@ -13,7 +12,6 @@ background-color: #749f5d;
 <br>
 <h2> New here? Click <a href="tree_form.php"> here</a> to sign up! </h2>
 
-</style>
 </div>
 </body>
 </html>
