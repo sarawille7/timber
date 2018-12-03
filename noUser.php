0ee133e2c0ed682525fc2ebc3adb3ecd505cba07
Benjamin Scarbrough <Scarbrough.ben@gmail.com>
2018-12-03 12:17:36 -0800

    added a noUser page

---
+++ noUser.php
@@ -0,0 +1,9 @@
+<?php
+<head><link rel="stylesheet" type="text/css" href="basic.css"></head>
+
+echo("No user exists with that username or password.");
+echo"<a href='$login.php'>Try Again</a>";
+
+echo"<a href='$user_form.php'>Try Again</a>";
+
+ ?>
