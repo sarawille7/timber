9ef24771151a97f87cd6cb8a17f8147ff3d226fa
Benjamin Scarbrough <Scarbrough.ben@gmail.com>
2018-12-03 12:46:26 -0800

    forgot something

--- noUser.php
+++ noUser.php
@@ -8,10 +8,10 @@ include("menu.PHP");
 <div class = "main">
   <h1> Error: Something went wrong! </h1>
 <br>
-  <h2 href="login.php"> Try Again </h2>
+  <a href="login.php"> Try Again </a>
 <br>
-<h2> "New here? Click" <h2 href="tree_form.php"> here </h2> to make an account! </h2>
-</div>
+<h2> New here? Click <a href="tree_form.php"> here </a> to make an account! </h2>
 
+</div>
 </body>
 </html>
